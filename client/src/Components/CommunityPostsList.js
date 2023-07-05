f8f98e6d74b326d5284ae53bfd1f45ff2706ad0a
lranatunga <rmlik.ranatunga@gmail.com>
2023-07-05 14:14:05 +0100

    fech community posts

--- client/src/Components/CommunityPostsList.js
+++ client/src/Components/CommunityPostsList.js
@@ -2,23 +2,30 @@ import useFetchData from "../CustomHooks/useFetchData"
 import TimeAgo from 'react-timeago'
 
 export default function ListCommunityPost () {
-    const {data, error} = useFetchData(`http://localhost:5000/community/listpost`)
+    const {data} = useFetchData(`http://localhost:5000/community/listpost`)
     console.log(data)
     const post = data.communityPosts
 
     console.log("posts:", post)
+
+    if (!data){
+        return (
+            <div>Loading</div>
+        )
+    }
 
     return (
         <div className="community_post_list">
             <div>community posts</div>
             {
                 post.map((item) => {
-                    const {_id, owner, text, date } = item
+                    const {_id, owner, text, date} = item
                     return (
                         <div key={_id}>
                             <div className="post-owner" style={{fontSize:"1.2rem", fontWeight:"bold"}}>
                                 {owner.firstName} <span>{owner.lastName}</span>
                             </div>
+
                             <div className="post-topic">
                                 {text}
                             </div>
