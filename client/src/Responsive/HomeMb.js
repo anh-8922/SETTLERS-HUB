e06685ed974e27cd90484d60a073748e27bc133d
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-07-10 05:07:31 +0100

    update responsive HomePage

---
+++ client/src/Responsive/HomeMb.js
@@ -0,0 +1,94 @@
+import londonA from '../Assets/londonA.jpg';
+import '../Style/responsive.css';
+import logo1 from '../Assets/logo1.png';
+import MenuBarMb from './MenuBarMb';
+import {BsChevronCompactDown} from 'react-icons/bs';
+import { useRef } from 'react';
+import NewsTicker from 'react-advanced-news-ticker';
+import useFetchData from "../CustomHooks/useFetchData";
+import {SpotlightsCard} from "../Features/SummaryCard";
+import Spinner from "../Features/Spinner";
+import { useNavigate } from "react-router-dom";
+import CategoryMb from './CategoryMb';
+import FooterMb from './FooterMb';
+import LayoutMB from './LayoutMB';
+
+export default function HomeMb() {
+    
+    return(
+        <>
+            <HeroSectionMb/>
+            <SpotLightMb/>
+            <CategoryMb/>
+            <FooterMb/>
+        </>
+    )
+}
+
+function HeroSectionMb() {
+    
+
+    return(
+        <div className="heroSectionMb" style={{
+            backgroundImage: `url("${londonA}")`, backgroundPosition:'contain',
+            textAlign:'center', 
+            height:'60vh'}}>
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', padding:'2rem 3rem'}}>
+                <img src={logo1} style={{width:'40%', height:'70%'}}/>
+                <MenuBarMb/>
+            </div>
+            <h1 style={{
+                display:"flex", 
+                flexDirection:"column", 
+                justifyContent: "center", 
+                height:'50%',
+                color:"whitesmoke"}}>
+            Some Main Text Should Be Here
+            </h1>
+        </div>
+    )
+}
+
+function SpotLightMb() {
+    const ref = useRef();
+    const { data } = useFetchData("http://localhost:5000/guide/list" );
+    // console.log("datafetched",data)
+    const navigate = useNavigate();
+  
+    const handleReadMore = (_id) => {
+        console.log("_id", _id)
+      navigate(`/singleguidepost/${_id}`);
+    };
+  
+    if (!data) {
+      return <Spinner />;
+    }
+    const spotlightCard = data.filter(
+      (item) => item.category.toLowerCase() === "spotlights"
+    );
+   console.log(spotlightCard)
+    return(
+        <div className='spotlight-news'>
+                        <NewsTicker style={{height:"30rem", listStyle:"none"}} 
+                            rowHeight={150} 
+                            ref={ref} 
+                            duration={6000}>
+                            {spotlightCard.map((item) => {
+                            const { _id, image, title, abstract } = item;
+                                return (
+                                    <SpotlightsCard
+                                    key={_id}
+                                    title={title}
+                                    abstract={abstract}
+                                    image={image}
+                                    onHandleClick={() => handleReadMore(_id)}
+                                    />
+                                );
+                            })}
+                        </NewsTicker>
+                        <BsChevronCompactDown style={{fontSize:"3rem", cursor:'pointer'}} 
+                        onClick={() => { ref.current.moveNext(); ref.current.resetInterval(); }}/>
+                    </div>
+    )
+}
+
---
+++ client/src/Responsive/MenuBarMb.js
@@ -0,0 +1,43 @@
+import {BsMenuUp} from 'react-icons/bs';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../Style/responsive.css';
+
+export default function MenuBarMb() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+    return(
+        <div style={{
+            backgroundColor:'whitesmoke', 
+            borderRadius:'50%', 
+            
+            padding: '1rem'}}>
+                <BsMenuUp style={{fontSize:'2.5rem'}} onClick={handleOpen}/>
+                <Modal
+                open={open}
+                onClose={handleClose}
+                
+                >
+                    <Box style={{
+                        backgroundColor:'whitesmoke', 
+                        margin:'7rem 1rem 0 15rem', height:'85vh', borderRadius:'1rem',
+                        display: 'flex', flexDirection:'column',
+                        gap:'1rem', alignItems:'flex-start', alignContent:'stretch', padding:'2rem'}}>
+                        <Link className='MenuMb-items' to='/'>Home</Link>
+                        <Link className='MenuMb-items' to="/service">Service</Link>
+                        <Link className='MenuMb-items'>Housing</Link>
+                        <Link className='MenuMb-items' to="/community">Community</Link>
+                        <Link className='MenuMb-items' to="/userregister">Signup</Link>
+                        <Link className='MenuMb-items' to="/login">Login</Link>
+                    </Box>
+                </Modal>
+        </div>
+    )
+}
