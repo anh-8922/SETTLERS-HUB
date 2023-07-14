ee05583fed78ff3376b812ccf4b171e6cd02656b
lranatunga <rmlik.ranatunga@gmail.com>
2023-07-14 19:21:24 +0100

    profile

--- client/src/Features/MyProfile.js
+++ client/src/Features/MyProfile.js
@@ -1,15 +1,54 @@
 import useFetchData from "../CustomHooks/useFetchData"
 import { useGetUserID } from "../CustomHooks/useGetUserID"
+import profile from "../Assets/profile.png"
+import Spinner from "./Spinner"
+import { useState } from "react"
 
 export default function MyProfile () {
     const userID = useGetUserID ()
     console.log("profileuser:", userID)
     const {data} = useFetchData(`http://localhost:5000/user/listoneuser/${userID}`)
     console.log("user data profile:", data)
+    const [profileUpdate, setProfileUpdate] = useState(false)
 
+    if (!data) {
+        return <Spinner />; 
+      }
+    
+    const handleUpdateProfile = () => {
+        setProfileUpdate(true)
+    }
+
+    const handleSaveProfile =() => {
+        setProfileUpdate(false)
+    }
+    const {firstName, lastName, email, username, telephone, address,} = data.selectedUser
+   
+    // const city = address[0].city
     return (
         <div>
-            hi my pro
+
+
+           { !profileUpdate ? (
+                        <div>
+                        <button onClick={handleUpdateProfile}>Update</button>
+                        <div>
+                            <img src={profile} alt="profile-image" style={{width:'200px', height:'200px'}}/>
+                        </div>
+                        
+                        <div>{firstName} {lastName}</div>
+                        <div>{username} </div>
+                        <div>{email} </div>
+                        <div>{telephone}</div>
+                        {/* <div>{city}</div> */}
+                        </div>
+                        
+           ):(
+            <div>
+                Hi profile update
+                <button onClick={handleSaveProfile}>Update</button>
+            </div>
+           ) }
         </div>
     )
 }
