76631677fecf224be1a4bf22079cbf6b2c885c83
lranatunga <rmlik.ranatunga@gmail.com>
2023-07-17 18:59:35 +0100

    community post delete

--- client/src/Features/CommunityPostsList.js
+++ client/src/Features/CommunityPostsList.js
@@ -1,87 +1,14 @@
-// import useFetchData from "../CustomHooks/useFetchData"
-// import TimeAgo from 'react-timeago'
-// import { useEffect, useState} from "react"
-// import '../Style/feature.css'
 
-// export default function ListCommunityPost () {
-//     const {data,error, refetch} = useFetchData(`http://localhost:5000/community/listpost`)
-//     console.log(data)
-//     const [posts, setPosts] = useState([])
-
-//     if (!data){
-//         return (
-//             <div>{error}</div>
-//         )
-//     }
-
-//     return (
-//         <div className="community_post_list">
-           
-//             {
-//                data.communityPosts.map((item) => {
-//                     const {_id, owner, text, createdAt, image} = item
-//                     return (
-//                         <div key={_id}>
-//                             <div className="post-owner" style={{fontSize:"1.2rem", fontWeight:"bold"}}>
-//                                 {owner.firstName} <span>{owner.lastName}</span>
-//                             </div>
-//                              {/* <div>
-//                                 <img src={`https://res.cloudinary.com/dgnqjr0we/image/upload/${image}`} alt={image}/>
-//                             </div>  */}
-
-//                             <div className="post-topic">
-//                                 {text}
-//                             </div>
-//                             <div>
-//                             <TimeAgo date={createdAt}/>
-//                             </div>
-
-//                         </div>
-//                     )
-
-//                 })
-//             }
-//         </div>
-//     )
-// }
-
-// import TimeAgo from 'react-timeago'
-// import '../Style/feature.css'
-
-// export default function ListCommunityPost ({props}) {
-//     const {_id, owner, text, createdAt, image} = props
-//     // const firstName= owner.firstName
-//     // const lastName = owner.lastName
-//     return (
-//         <div  key={_id} className="community_post_list">
-           
-           
-//                             <div className="post-owner" style={{fontSize:"1.2rem", fontWeight:"bold"}}>
-//                                 {owner} <span>{owner}</span>
-//                             </div>
-//                              {/* <div>
-//                                 <img src={`https://res.cloudinary.com/dgnqjr0we/image/upload/${image}`} alt={image}/>
-//                             </div>  */}
-
-//                             <div className="post-topic">
-//                                 {text}
-//                             </div>
-//                             <div>
-//                             <TimeAgo date={createdAt}/>
-//                             </div>
-
-                       
-            
-//         </div>
-//     )
-// }
 
 import TimeAgo from "react-timeago";
 import "../Style/feature.css";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { FaRegComments } from "react-icons/fa"
 
-export default function ListCommunityPost({ _id, firstName, lastName, createdAt, text }) {
+export default function ListCommunityPost({ _id, firstName, lastName, createdAt, handleLike, text, handleDeletePost, handleEditPost }) {
   return (
     <div key={_id} className="community_post_list">
+      <div><button onClick={() => handleDeletePost(_id)}>Edit</button> <button onClick={() => handleDeletePost(_id)}>Delete</button></div>
       <div className="post-owner" style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
         {firstName} <span>{lastName}</span>
       </div>
@@ -89,6 +16,21 @@ export default function ListCommunityPost({ _id, firstName, lastName, createdAt,
       <div>
         <TimeAgo date={createdAt} />
       </div>
+      <div>
+      <AiFillHeart
+                className="text-red-500 text-[2rem] cursor-pointer"
+                onClick={handleLike}
+              />
+              <span>Like</span>
+
+        <AiOutlineHeart
+                className="text-red-500 text-[2rem] cursor-pointer"
+                onClick={handleLike}
+              />
+              <span>Liked</span>
+        <FaRegComments className="text-slate-500 hover:text-red-500 text-[2rem] cursor-pointer" />
+      </div>
+      <div>comment</div>
     </div>
   );
 }
